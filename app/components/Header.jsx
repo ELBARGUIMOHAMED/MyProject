f874cbce34bfc43b4287a29cba4a1f302418cfdd
MOHAMED ELBARGUI <114398140+ELBARGUIMOHAMED@users.noreply.github.com>
2025-01-22 02:54:41 +0100

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div
       className="w-11/12 max-w-3xl text-center mx-auto h-screen 
-        flex flex-col items-center justify-center gap-4"
+        flex flex-col items-center justify-center gap-1"
     >
       <motion.div 
       initial={{scale: 0}} 
